44b21bdb4fa41a5258501bfc4586b3c863bfb0af
Mathieu Boisnard <mathieu.boisnard@lacombedulionvert.fr>
2021-03-30 23:03:51 +0200

    Add aws amplify react

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Amplify from 'aws-amplify';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import awsconfig from './aws-exports';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+Amplify.configure(awsconfig);
+
+const App = () => (
+    <div>
+      <AmplifySignOut />
+      My App
     </div>
-  );
-}
+);
 
-export default App;
+export default withAuthenticator(App);
